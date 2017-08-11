8ceb08a40c73f6d3e20257f25ce26edee5f64054
Hamid Rohani <hamid914@users.noreply.github.com>
2017-08-11 15:40:20 +0430

    fix errors to see success build

--- test_sample.py
+++ test_sample.py
@@ -1,8 +1,11 @@
 def inc(x):
     return x + 1
 
+def double(x):
+    return 2 * x
+
 def test_answer1():
     assert inc(4) == 5
 
 def test_answer2():
-    assert inc(3) == 5
+    assert double(3) == 6
